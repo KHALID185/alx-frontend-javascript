3fd841e2d8f1e23749dcea428d2e1504f4085f5d
KHALID185 <khalid.1998.taouti@gmail.com>
2024-10-03 10:40:07 -0700

    classes

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,20 +1,18 @@
-
 export default class HolbertonClass {
   constructor(size, location) {
-    if (typeof size !== 'number') {
-      throw new TypeError('Size must be a number');
-    } else if (typeof location !== 'string') {
-      throw new TypeError('Location must be a string');
-    }
-
+    // Create objs
     this._size = size;
     this._location = location;
   }
 
-  [Symbol.toPrimitive](hint) {
-    if (hint === 'number') {
-      return this._size;
-    }
-    return this._location;
+  // Methods
+
+  [Symbol.toPrimitive](type) {
+    if (type === 'string') return this._location;
+    return this._size;
   }
+
+  // Setters
+
+  // Getters
 }
